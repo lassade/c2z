421214dd47e287b7105cdf63a0ee526d5107215d
Felipe Jorge <felipe.jorge.pereira@gmail.com>
2023-07-07 18:28:27 -0300

    more tests

--- src/cpp.zig
+++ src/cpp.zig
@@ -76,118 +76,106 @@ pub fn Allocator(comptime T: type) type {
     };
 }
 
-/// MSVC proxy iterator, my guess is that is used in debug builds to invalidate iterators, look for `_MyProxy` in `xmemory`
-const ProxyIter = 
-    if (builtin.mode == .Debug)
+pub const native = switch (builtin.abi) {
+    .msvc => msvc,
+    else => gnu,
+};
+
+pub const msvc = struct {
+    /// MSVC `_Container_base12`, it might be used in debug builds to invalidate iterators
+    const Container =
+        if (builtin.mode == .Debug)
         extern struct {
             const Self = @This();
 
-            const ContainerProxy = struct {
-                cont: ?*const anyopaque = null,
-                first_iter: ?*anyopaque = null,
+            const Iter = extern struct {
+                proxy: ?*const ContainerProxy = null,
+                next: ?*Iter = null,
             };
 
-            container: ?*ContainerProxy = null,
-            
+            const ContainerProxy = extern struct {
+                cont: ?*const Self = null,
+                iter: ?*Iter = null,
+            };
+
+            proxy: ?*ContainerProxy,
+
             pub fn init() Self {
-                var container = @ptrCast(?*ContainerProxy, @alignCast(8, malloc(@sizeOf(ContainerProxy))));
-                container.?.* = .{};
-                return .{ .container = container };
+                var proxy = @ptrCast(?*ContainerProxy, @alignCast(8, malloc(@sizeOf(ContainerProxy))));
+                proxy.?.* = .{};
+                return .{ .proxy = proxy };
             }
 
-            pub fn deinit(self: *Self) void { 
+            pub fn deinit(self: *Self) void {
+                _ = self;
+                // todo: to free it you must walk the linked list
+                // todo: can't use `free(self.proxy);` on String
                 // todo: test if will leak memory when passing containers between ffi bounds
-                free(self.container);
             }
         }
     else
         extern struct {
             const Self = @This();
 
-            pub fn init() Self { return .{}; }
+            pub fn init() Self {
+                return .{};
+            }
             pub fn deinit(_: *Self) void {}
         };
 
-/// basic `std::vector` compatible type, it doesn't free items
-pub fn Vector(comptime T: type) type {
-    return VectorRaw(T, Allocator(T), .{});
-}
+    /// basic `std::vector` compatible type, it doesn't free items
+    pub fn Vector(comptime T: type) type {
+        return msvc.VectorRaw(T, Allocator(T));
+    }
 
-/// base type for any `std::vector` derived type  with a custom allocator type and other configurations, it doesn't free items
-pub fn VectorRaw(
-    comptime T: type,
-    comptime Alloc: type,
-    comptime config: struct {
-        /// support `msvc`, requires at least `-O1` to work
-        msvc: bool = builtin.abi == .msvc,
-    },
-) type {
-    const Data = if (config.msvc)
-        extern struct { __proxy: ProxyIter, allocator: Alloc, head: ?*T = null, tail: ?*T = null, limit: ?*T = null, }
-    else
-        extern struct { head: ?*T = null, tail: ?*T = null, limit: ?*T = null, allocator: Alloc, };
+    /// base type for any `std::vector` derived type  with a custom allocator type and other configurations, it doesn't free items
+    pub fn VectorRaw(comptime T: type, comptime Alloc: type) type {
+        return extern struct {
+            const Self = @This();
 
-    return extern struct {
-        const Self = @This();
+            __proxy: Container,
+            allocator: Alloc,
+            head: ?*T = null,
+            tail: ?*T = null,
+            limit: ?*T = null,
 
-        data: Data,
-
-        pub fn init(allocator: Alloc) Self {
-            var self: Self = undefined;
-            if (config.msvc) self.data.__proxy = ProxyIter.init();
-            self.data.allocator = allocator;
-            self.data.head = null;
-            self.data.tail = null;
-            self.data.limit = null;
-            return self;
-        }
-
-        pub inline fn size(self: *const Self) usize {
-            return (@ptrToInt(self.data.tail) - @ptrToInt(self.data.head));
-        }
-
-        pub inline fn capacity(self: *const Self) usize {
-            return (@ptrToInt(self.data.limit) - @ptrToInt(self.data.head));
-        }
-
-        pub inline fn values(self: Self) []T {
-            return if (self.data.head) |head| @ptrCast([*]T, head)[0..self.size()] else &[_]T{};
-        }
-
-        pub fn deinit(self: *Self) void {
-            if (self.data.head) |head| {
-                self.data.allocator.deallocate(head, self.size());
-
-                self.data.head = null;
-                self.data.tail = null;
-                self.data.limit = null;
+            pub fn init(allocator: Alloc) Self {
+                return .{
+                    .__proxy = Container.init(),
+                    .allocator = allocator,
+                };
             }
-        }
-    };
-}
 
-/// just use `[N]T`
-pub fn Array(
-    comptime T: type,
-    comptime N: comptime_int,
-) type {
-    return @Type([N]T);
-}
+            pub inline fn size(self: *const Self) usize {
+                return (@ptrToInt(self.tail) - @ptrToInt(self.head));
+            }
 
-// todo: cpp.String uses a inline string
+            pub inline fn capacity(self: *const Self) usize {
+                return (@ptrToInt(self.limit) - @ptrToInt(self.head));
+            }
 
-/// drop-in replacement for `std::string`
-pub const String = StringRaw(Allocator(u8), .{});
+            pub inline fn values(self: Self) []T {
+                return if (self.head) |head| @ptrCast([*]T, head)[0..self.size()] else &[_]T{};
+            }
 
-/// similar to `std::basic_string<char, std::char_traits<char>, Alloc>`
-pub fn StringRaw(
-    comptime Alloc: type,
-    comptime config: struct {
-        /// support `msvc`, requires at least `-O1` to work
-        msvc: bool = builtin.abi == .msvc,
-    },
-) type {
-    if (config.msvc) {
+            pub fn deinit(self: *Self) void {
+                if (self.head) |head| {
+                    self.allocator.deallocate(head, self.size());
+
+                    self.head = null;
+                    self.tail = null;
+                    self.limit = null;
+                }
+                self.__proxy.deinit();
+            }
+        };
+    }
+
+    /// drop-in replacement for `std::string`
+    pub const String = msvc.StringRaw(Allocator(u8));
+
+    /// similar to `std::basic_string<char, std::char_traits<char>, Alloc>`
+    pub fn StringRaw(comptime Alloc: type) type {
         const Heap = extern struct {
             ptr: [*]u8,
             __payload: usize,
@@ -201,20 +189,20 @@ pub fn StringRaw(
         return extern struct {
             const Self = @This();
 
-            __proxy: ProxyIter,
+            __proxy: Container,
             allocator: Alloc,
             data: Data,
             len: usize,
             cap: usize,
 
             pub fn init(allocator: Alloc) Self {
-                var self: Self = undefined;
-                if (config.msvc) self.__proxy = ProxyIter.init();
-                self.allocator = allocator;
-                self.data = undefined;
-                self.len = 0;
-                self.cap = @sizeOf(Heap) - 1;
-                return self;
+                return .{
+                    .__proxy = Container.init(),
+                    .allocator = allocator,
+                    .data = undefined,
+                    .len = 0,
+                    .cap = @sizeOf(Heap) - 1,
+                };
             }
 
             inline fn inHeap(self: *const Self) bool {
@@ -241,9 +229,61 @@ pub fn StringRaw(
                     self.allocator.deallocate(@ptrCast(*u8, self.data.heap.ptr), self.cap);
                     self.data.in_place[0] = 0;
                 }
+                self.__proxy.deinit();
             }
         };
-    } else {
+    }
+};
+
+pub const gnu = struct {
+    /// basic `std::vector` compatible type, it doesn't free items
+    pub fn Vector(comptime T: type) type {
+        return gnu.VectorRaw(T, Allocator(T));
+    }
+
+    /// base type for any `std::vector` derived type  with a custom allocator type and other configurations, it doesn't free items
+    pub fn VectorRaw(comptime T: type, comptime Alloc: type) type {
+        return extern struct {
+            const Self = @This();
+
+            head: ?*T = null,
+            tail: ?*T = null,
+            limit: ?*T = null,
+            allocator: Alloc,
+
+            pub fn init(allocator: Alloc) Self {
+                return .{ .allocator = allocator };
+            }
+
+            pub inline fn size(self: *const Self) usize {
+                return (@ptrToInt(self.tail) - @ptrToInt(self.head));
+            }
+
+            pub inline fn capacity(self: *const Self) usize {
+                return (@ptrToInt(self.limit) - @ptrToInt(self.head));
+            }
+
+            pub inline fn values(self: Self) []T {
+                return if (self.head) |head| @ptrCast([*]T, head)[0..self.size()] else &[_]T{};
+            }
+
+            pub fn deinit(self: *Self) void {
+                if (self.head) |head| {
+                    self.allocator.deallocate(head, self.size());
+
+                    self.head = null;
+                    self.tail = null;
+                    self.limit = null;
+                }
+            }
+        };
+    }
+
+    /// drop-in replacement for `std::string`
+    pub const String = gnu.StringRaw(Allocator(u8));
+
+    /// similar to `std::basic_string<char, std::char_traits<char>, Alloc>`
+    pub fn StringRaw(comptime Alloc: type) type {
         const Heap = extern struct {
             cap: usize,
             len: usize,
@@ -299,6 +339,21 @@ pub fn StringRaw(
             }
         };
     }
+};
+
+// todo: try std::array in msvc, I think it works differently in debug
+/// just use `[N]T`
+pub fn Array(
+    comptime T: type,
+    comptime N: comptime_int,
+) type {
+    return @Type([N]T);
 }
+
+pub const Vector = native.Vector;
+pub const VectorRaw = native.VectorRaw;
+
+pub const String = native.String;
+pub const StringRaw = native.StringRaw;
 
 // todo: UniquePtr, SharedPtr
